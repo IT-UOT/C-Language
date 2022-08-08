c256d2d997edfffc08ae560e6fb87e1200b09ff7
mohamed-benoba <54261592+mohamed-benoba@users.noreply.github.com>
2022-08-08 17:18:13 +0200

    Create المؤشرات2.c

---
+++ المؤشرات2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+  #include <string.h>
+
+  /* 
+    Pointers/2
+    أكتب برنامج يطلب ادخال رقمين من المستخدم ويجمعهم وذلك باستخدام المؤشرات
+  */
+
+  int main() {
+    float num1, num2, sum;
+    float *ptr1, *ptr2;
+    ptr1 = &num1;
+    ptr2 = &num2;
+
+    puts("Input Two Numbers:");
+    scanf("%f %f", ptr1, ptr2);
+    sum = *ptr1 + *ptr2;
+
+    printf("Sum = %.2f\n", sum);
+  }
