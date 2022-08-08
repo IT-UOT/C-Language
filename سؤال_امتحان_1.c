b1f87b0d9c07615a1536dce2075b291f03735a7d
mohamed-benoba <54261592+mohamed-benoba@users.noreply.github.com>
2022-08-08 16:21:45 +0200

    Create سؤال_امتحان_1.c

---
+++ سؤال_امتحان_1.c
@@ -0,0 +1,17 @@
+  #include <stdio.h>
+  #include <string.h>
+  #include <math.h>
+
+  /* 
+    السؤال:
+    أكتب برنامج يقوم بإيجاد قيمة d
+    d = √((P+R+A)^3 - T^3)
+  */
+
+  int main() {
+    float P, R, A, T, d;
+    puts("Enter P, R, A, T");
+    scanf("%f %f %f %f", &P, &R, &A, &T);
+    d = sqrt(pow((P+R+T), 3) + pow(T, 3));
+    printf("%f", d);
+  }
